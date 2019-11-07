4f335451e0c9aec25ad25f43f2758ec1c071911b
Luiz <designarttdev@gmail.com>
2019-11-07 01:14:50 -0300

    Balta Io aula 16

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: any[] = [];
+  public title: String = 'Minhas Tarefas';
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required,
+      ])]
+    });
+    
+    this.todos.push(new Todo(1, 'Passear', false));
+    this.todos.push(new Todo(2, 'Ir na mamãe', false));
+    this.todos.push(new Todo(3, 'Estudar na faculdade', false));
+    this.todos.push(new Todo(4, 'Trabalhar na gráfica', true));
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if(index !== -1){
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+  }
 }
---
+++ todo/src/models/todo.model.ts
@@ -0,0 +1,11 @@
+export class Todo {
+    
+    constructor(
+        public id: number,
+        public title: String,
+        public done: boolean,
+        
+    ) {
+
+    }
+}
